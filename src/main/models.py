f2b17c20f880fb5050033dead4c8df5e15dfb532
Julian Erdmannsdörfer <julian.erdmannsdoerfer@sogeti.com>
2021-06-12 10:46:44 +0200

    Added DB entries for the players.

--- src/main/models.py
+++ src/main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Winner(models.Model):
+    times_won = models.IntegerField()
+    times_lost = models.IntegerField()
+    winner = models.ForeignKey(User, on_delete=models.CASCADE)
--- src/main/views.py
+++ src/main/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.db.models import Max
+from .models import Winner
+
+
 
 def home(request):
+    winners = Winner.object.all()
+    num_players = winners.count()
+    best_player = winners.aggregate(Max('times_won'))
+    #this_player = winners.filter(winner=).first()
     context = {
-        'title': 'This'
+        'title': 'This',
+        'num_players': num_players,
+        'best_player': best_player,
+        'winners': winners,
     }
     return render(request, 'main/main.html', context)
