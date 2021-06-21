a1dc1adb4028de7759cdb19242a729ad3e854944
Julian Erdmannsdörfer <julian.erdmannsdoerfer@sogeti.com>
2021-06-21 19:25:51 +0200

    Removed the unnecessary winner model.

--- src/main/models.py
+++ src/main/models.py
@@ -1,7 +1,4 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Winner(models.Model):
-    times_won = models.IntegerField()
-    times_lost = models.IntegerField()
-    winner = models.ForeignKey(User, on_delete=models.CASCADE)
+# No models needed for now
